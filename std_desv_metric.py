0e2e323d646797a03e30a868306d6cac697b8987
eduardo <eduardo9871@gmx.es>
2020-06-25 03:06:20 -0500

    discretizacion

--- std_desv_metric.py
+++ std_desv_metric.py
@@ -2,7 +2,8 @@ import pandas as pd
 import math
 from functools import reduce
 
-spec_power_data_file = 'Data/winequality-red2.xlsx'
+spec_power_data_file = 'Data/winequality-red3.xlsx'
+spec_power_data_file2 = 'Data/winequality-red4.xlsx'
 reader = pd.read_excel(spec_power_data_file, header=0)
 columns = reader.columns
 data_csv = reader._get_values
@@ -89,31 +90,88 @@ def gainSplit(reader, father_index, column_index):
     father_entropy = attribute_summarize[father_index]['entropy']
     count_list = list(map(lambda x: x['count'], attribute_summarize[father_index]['disctint_values_name']))
     total = reduce(lambda a, b: a + b, count_list)
-    sum=0
+    sum = 0
     for obj in disctint_values:
-        sum+=(obj['count']/total*obj['entropy'])
-    attribute_summarize[column_index]['gain_split']=father_entropy-sum
+        sum += (obj['count'] / total * obj['entropy'])
+    attribute_summarize[column_index]['gain_split'] = father_entropy - sum
+
 
 def splitInfo(reader, father_index, column_index):
     disctint_values = attribute_summarize[column_index]['disctint_values_name']
     father_entropy = attribute_summarize[father_index]['entropy']
     count_list = list(map(lambda x: x['count'], attribute_summarize[father_index]['disctint_values_name']))
     total = reduce(lambda a, b: a + b, count_list)
-    sum=0
+    sum = 0
     for obj in disctint_values:
         pi = obj['count'] / total
         if pi > 0:
             sum += (pi * math.log2(pi))
-    attribute_summarize[column_index]['split_info']=-1*sum
+    attribute_summarize[column_index]['split_info'] = -1 * sum
+
 
 def gainRatio(reader, column_index):
-    gainsplit=attribute_summarize[column_index]['gain_split']
-    splitinfo=attribute_summarize[column_index]['split_info']
-    if splitinfo==0:
-        splitinfo=1
-    attribute_summarize[column_index]['gain_ratio']=gainsplit/splitinfo
+    gainsplit = attribute_summarize[column_index]['gain_split']
+    splitinfo = attribute_summarize[column_index]['split_info']
+    if splitinfo == 0:
+        splitinfo = 1
+    attribute_summarize[column_index]['gain_ratio'] = gainsplit / splitinfo
 
 
+def dicretize(reader, column_index):
+    variablefield = []
+    classfield = []
+    for i in reader[columns[column_index]]:
+        variablefield.append(i)
+    for i in reader[columns[-1]]:
+        classfield.append(i)
+
+    for i in range(len(variablefield) - 1):
+        for j in range(i + 1, len(variablefield)):
+            if variablefield[i] > variablefield[j]:
+                aux1 = variablefield[i]
+                aux2 = classfield[i]
+                variablefield[i] = variablefield[j]
+                classfield[i] = classfield[j]
+                variablefield[j] = aux1
+                classfield[j] = aux2
+
+    antecesor = None
+    nuevo = True
+    min = float('-inf')
+    max_antecesor=float('-inf')
+    max = variablefield[0]
+    grupos = []
+    print(variablefield)
+    print(classfield)
+    for i in range(1, len(variablefield)):
+        if classfield[i] == classfield[i - 1]:
+            if variablefield[i] != variablefield[i - 1]:
+                max_antecesor = (variablefield[i-1])
+            max = (variablefield[i])
+        else:
+            if variablefield[i] == variablefield[i - 1]:
+                max=max_antecesor
+            grupos.append({'min': min, 'max': max})
+            min=max
+            max=variablefield[i]
+    if len(grupos)>0:
+        grupos.append({'min': grupos[-1]['max'], 'max': float('inf')})
+    else:
+        grupos.append({'min': float('-inf'), 'max': float('inf')})
+
+    print(grupos)
+    data=[]
+    for row in reader[columns[column_index]]:
+        for grupo in grupos:
+            if row > grupo['min'] and row <= grupo['max']:
+                data.append(str(grupo['min'])+"-"+str(grupo['max']))
+
+    reader[columns[column_index]] = data
+    reader.to_excel(spec_power_data_file2, index=False)
+
+for i in range(len(columns) - 1):
+    dicretize(reader, i)
+"""
 for column in columns:
     obtainMetrics(reader, column)
 fatherEntropy(reader,-1)
@@ -126,3 +184,4 @@ for i in range(len(columns) - 1):
 for i in range(len(columns) - 1):
     gainRatio(reader, i)
 print(attribute_summarize)
+"""
